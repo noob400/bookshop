7ff5c86603cef554e3b5b55af20b9994d5451d10
kelthuzadx <user.name>
2019-05-07 15:46:04 +0800

    [ Shiro] Remove shiro authentication framework

--- src/main/java/com/daniel/exception/DefaultExceptionHandler.java
+++ src/main/java/com/daniel/exception/DefaultExceptionHandler.java
@@ -1,22 +1,25 @@
 package com.daniel.exception;
 
-import org.apache.shiro.authz.UnauthorizedException;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@ControllerAdvice
-public class DefaultExceptionHandler {
-    @ExceptionHandler({UnauthorizedException.class})
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ModelAndView processUnauthenticatedException(NativeWebRequest request, UnauthorizedException e) {
-        ModelAndView mv = new ModelAndView();
-        mv.addObject("ex", e);
-        mv.setViewName("unauthorized");
-        return mv;
+
+@Component
+public class DefaultExceptionHandler implements HandlerExceptionResolver {
+
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                         Exception ex) {
+        System.out.println("This is exception handler method!");
+        return null;
     }
 }
